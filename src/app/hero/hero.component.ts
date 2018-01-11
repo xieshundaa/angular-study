1b49a415893c80ae8971beedd1d0deb0377eeae6
xieshundaa <17743566997@163.com>
2018-01-11 10:48:23 +0800

    fix

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent implements OnInit {
-  constructor() {}
+  constructor(public router: Router) {}
 
   ngOnInit() {
+  }
+  gohome() {
+    this.router.navigateByUrl('home');
   }
 }
